aa08138a50d399414d6b3a3d4b47c52eb6cf14d8
SudarsonArulnithi <asudarshine@gmail.com>
2018-05-24 07:42:42 +0100

    1

---
+++ REVandCOD/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace REVandCOD
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ REVandCOD/Controllers/NewOppurtunityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using REVandCOD.Models;
+
+namespace REVandCOD.Controllers
+{
+    public class NewOppurtunityController : Controller
+    {
+        NewOppurtunity oppur = new NewOppurtunity()
+        {
+            AccountName = "Npower"
+        };
+        // GET: NewOppurtunity
+        public ActionResult NewOpp()
+        {
+            return View(oppur);
+        }
+
+
+    }
+}
