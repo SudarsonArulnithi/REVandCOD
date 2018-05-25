ddf8094b4b829a0a087efabe78feb2b898b18285
SudarsonArulnithi <asudarshine@gmail.com>
2018-05-25 12:24:23 +0530

    oppurtunity cs

---
+++ REVandCOD/Controllers/OppurtunityController.cs
@@ -0,0 +1,28 @@
+﻿using REVandCOD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace REVandCOD.Controllers
+{
+    public class OppurtunityController : Controller
+    {
+        // GET: Oppurtunity
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewOpp()
+        {
+            Oppurtunity oppur = new Oppurtunity()
+            {
+                AccountName="Sudarson"
+            };
+
+            return View(oppur);
+        }
+    }
+}
