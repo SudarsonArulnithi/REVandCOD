56d92755d806f6ef4ef4f13bb5d3876925ec323c
SudarsonArulnithi <asudarshine@gmail.com>
2018-05-25 18:23:37 +0530

    DropDown

--- REVandCOD/Controllers/NewOppurtunityController.cs
+++ REVandCOD/Controllers/NewOppurtunityController.cs
@@ -1,6 +1,8 @@
 ﻿using REVandCOD.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,6 +20,41 @@ namespace REVandCOD.Controllers
         [HttpPost]
         public ActionResult Create(NewOppurtunity newOppur)
         {
+            var con = ConfigurationManager.ConnectionStrings["REVENUE"].ToString();
+
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                string oString = "SELECT * FROM[RevenueDB].[dbo].[Oppurtunity]";
+                SqlCommand oCmd = new SqlCommand(oString, myConnection);
+                myConnection.Open();
+                using (SqlDataReader oReader = oCmd.ExecuteReader())
+                {
+                    while (oReader.Read())
+                    {
+                        Oppurtunity oppur = new Oppurtunity();
+
+                        oppur.AccountName = oReader["AccountName"].ToString();
+                        oppur.Portfolio = oReader["Portfolio"].ToString();
+                        oppur.OppurtunityType = oReader["OppurtunityType"].ToString();
+                        oppur.OppurtunityNo = oReader["OppurtunityNo"].ToString();
+                        oppur.OppurtunityName = oReader["OppurtunityName"].ToString();
+                        oppur.OppurtunityDescription = oReader["OppurtunityDescription"].ToString();
+                        oppur.ProjectType = oReader["ProjectType"].ToString();
+                        oppur.RFPDT = Convert.ToDateTime(oReader["RFPDT"]);
+                        oppur.ProposalSubmissionDate = Convert.ToDateTime(oReader["ProposalSubmissionDate"]);
+                        oppur.NegitationClosureDate = Convert.ToDateTime(oReader["NegitationClosureDate"]);
+                        oppur.LOIDT = Convert.ToDateTime(oReader["LOIDT"]);
+                        oppur.WinningProbability = Convert.ToInt64(oReader["WinningProbability"]);
+                        oppur.OnDateClosureProbability = Convert.ToInt64(oReader["OnDateClosureProbability"]);
+                        oppur.SignatureExpectedDate = Convert.ToDateTime(oReader["SignatureExpectedDate"]);
+
+                        model.Add(oppur);
+                    }
+
+                    myConnection.Close();
+                }
+            }
+
             return View();
         }
 
